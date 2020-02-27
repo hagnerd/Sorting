8c91ec10ef31af1ae9065795fe977b9e3a6e72a0
Matt Hagner <web@matthagner.com>
2020-02-26 20:18:23 -0600

    Solve Stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -30,5 +30,27 @@ def bubble_sort( arr ):
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    if len(arr) == 0:
+        return []
 
-    return arr
+    arr_max = max(arr) + 1
+    aux = [0] * arr_max
+    maths = [0] * arr_max
+    output = []
+
+    for i in range(0, len(arr)):
+        if arr[i] < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        aux[arr[i]] += 1
+
+    for j in range(0, arr_max - 1):
+        maths[j] = aux[j] if j == 0 else aux[j] + aux[j - 1]
+
+    for k in range(0, arr_max):
+        x = aux[k]
+        while x > 0:
+            output.append(k)
+            x -= 1
+
+
+    return output
